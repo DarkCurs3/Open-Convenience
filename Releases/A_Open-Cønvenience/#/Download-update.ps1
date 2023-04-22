2ca5d705609387f006c510a91c6ba13b1e6899f2
brissonsean <77543961+brissonsean@users.noreply.github.com>
2023-04-21 21:09:38 -0400

    Add files via upload

---
+++ Releases/A_Open-Cønvenience/#/Download-update.ps1
@@ -0,0 +1,7 @@
+@echo on
+:: Yabber ::
+.\#Download-Yabber.ps1
+:: UXM ::
+.\#Download-UXM.ps1
+
+pause
--- Releases/A_Open-Cønvenience/#Download-update.bat
+++ Releases/A_Open-Cønvenience/#Download-update.bat
@@ -1,6 +1,14 @@
 @echo on
+echo We need to do :powershell Set-ExecutionPolicy Unrestricted: to run the scripts; the script will download a script to re-enable this security at the end of the job
+echo if you didnt, run as administrator
+
+powershell Set-ExecutionPolicy Unrestricted
+
+
 echo Will execute 'powershell #Download-update.bat.ps1'
 
-powershell \#Download-update.bat.ps1
-pause
+powershell " .\#\Download-update.bat.ps1"
+powershell " .\#\Download-Yabber.ps1"
+powershell " .\#\Download-UXM.ps1"
+
 exit
