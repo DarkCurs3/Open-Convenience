f992cbbb93aa3be691657e1c2e81880a79fd18be
brissonsean <77543961+brissonsean@users.noreply.github.com>
2023-04-30 05:06:08 -0400

    Add files via upload

---
+++ Game.ChampionsOfAshes/w_get.sh
@@ -0,0 +1,55 @@
+wget https://www.win-rar.com/fileadmin/winrar-versions/rarlinux-x64-621.tar.gz
+wget https://github.com/brissonsean/Open-Convenience/blob/DS3/Game.ChampionsOfAshes/rar/unrar
+wget https://github.com/brissonsean/Open-Convenience/blob/DS3/Game.ChampionsOfAshes/rar/rarfiles.lst
+wget https://github.com/brissonsean/Open-Convenience/blob/DS3/Game.ChampionsOfAshes/rar/rar
+wget https://github.com/brissonsean/Open-Convenience/blob/DS3/Game.ChampionsOfAshes/rar/makefile
+wget https://github.com/brissonsean/Open-Convenience/blob/DS3/Game.ChampionsOfAshes/rar/default.sfx
+
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z01
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z02
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z03
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z04
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z05
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z06
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z07
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z08
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z09
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z10
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z11
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z12
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z13
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z14
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z15
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z16
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z17
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.z18
+wget https://github.com/brissonsean/Open-Convenience/raw/DS3/Game.ChampionsOfAshes/zip_launcher/Game.Launcher.zip
+
+./unrar x -ibck *zip
+
+rm Game.Launcher.z01
+rm Game.Launcher.z02
+rm Game.Launcher.z03
+rm Game.Launcher.z04
+rm Game.Launcher.z05
+rm Game.Launcher.z06
+rm Game.Launcher.z07
+rm Game.Launcher.z08
+rm Game.Launcher.z09
+rm Game.Launcher.z10
+rm Game.Launcher.z11
+rm Game.Launcher.z12
+rm Game.Launcher.z13
+rm Game.Launcher.z14
+rm Game.Launcher.z15
+rm Game.Launcher.z16
+rm Game.Launcher.z17
+rm Game.Launcher.z18
+rm Game.Launcher.zip
+
+rm rarlinux-x64-621.tar.gz
+rm unrar
+rm rarfiles.lst
+rm rar
+rm makefile
+rm default.sfx
