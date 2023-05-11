6e49d7267fe9e9c4a597c357855bcf570e83f6de
brissonsean <77543961+brissonsean@users.noreply.github.com>
2023-05-11 10:47:23 -0400

    Create download.sh

---
+++ Releases/download.sh
@@ -0,0 +1,52 @@
+
+#Timed Block#					chr/c0000-anibnd-dcx/
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a00.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a02.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a03.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a10.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a12.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a13.tae
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/TimedBlock/Timedblock/%23mod-mergable/chr/c0000-anibnd-dcx/GR/data/INTERROOT_win64/chr/c0000/tae/a15.tae
+
+
+# Ranni Spicy #					chr/
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Ranni/Ranni%20retextures/Spicy%20version/mod/chr.z01
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Ranni/Ranni%20retextures/Spicy%20version/mod/chr.z02
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Ranni/Ranni%20retextures/Spicy%20version/mod/chr.z03
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Ranni/Ranni%20retextures/Spicy%20version/mod/chr.zip
+
+# Melina goth #						chr/
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Melina/Melina%20Retexture/Goth%20Melina/mod/chr/c2180.chrbnd.dcx
+wget https://raw.githubusercontent.com/brissonsean/Open-Convenience/EldenRing/Mergum/%23MODS/3D/3D_Characters/Melina/Melina%20Retexture/Goth%20Melina/mod/chr/c2180_h.texbnd.dcx
+
+# Dark Skull Torrent horse # 		chr/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Characters/torrent/Dark%20Skull%20Knight%20Horse/mod/chr.z01
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Characters/torrent/Dark%20Skull%20Knight%20Horse/mod/chr.zip
+
+
+# Sauron Armor #		parts/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z01
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z02
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z03
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z04
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z05
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.z06
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Prelate%20Set/Dark%20Knight%20Sauron/Sauron%203.5/mod/parts.zip
+
+#ranno Black snowWitch Set# 	parts/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Ranni%20Set/Black%20Snow%20Witch%20set/mod/parts/bd_f_2200.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Ranni%20Set/Black%20Snow%20Witch%20set/mod/parts/bd_m_2200.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Ranni%20Set/Black%20Snow%20Witch%20set/mod/parts/hd_m_2200.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_EquipArmor/Ranni%20Set/Black%20Snow%20Witch%20set/mod/parts/lg_m_2200.partsbnd.dcx
+
+#ahego uchigatana 			parts/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/Uchiganata/Ahegao%20Uchigatana/mod/parts/wp_a_0550.partsbnd.dcx
+
+#FrostMourneLuciferx10#	parts/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/FrostMourne/Frostmourne.zLuciferX10%20Version/mod/parts/wp_a_0200.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/FrostMourne/Frostmourne.zLuciferX10%20Version/mod/parts/wp_a_0201.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/FrostMourne/Frostmourne.zLuciferX10%20Version/mod/parts/wp_a_0617.partsbnd.dcx
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/FrostMourne/Frostmourne.zLuciferX10%20Version/mod/parts/wp_a_0621.partsbnd.dcx
+
+#HolyAshbringner#	parts/
+wget https://github.com/brissonsean/Open-Convenience/raw/EldenRing/Mergum/%23MODS/3D/3D_Weapons3D/Holy%20Ashbringer/Holy%20Ashbringer.Sacred%20Relic%20Sword/Holy%20Ashbringer-Normal%20handle/mod/parts/wp_a_0618.partsbnd.dcx
